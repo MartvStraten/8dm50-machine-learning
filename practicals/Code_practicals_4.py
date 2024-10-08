5793282625feb707b8cce97df0ed5ce5d72889f2
Mart van Straten <131674849+MartvStraten@users.noreply.github.com>
2024-10-08 21:56:04 +0200

    Update

--- practicals/Code_practicals_4.py
+++ practicals/Code_practicals_4.py
@@ -1,7 +1,7 @@
 import numpy as np
 import gryds
 
-def data_augmentation(X_train, y_train, prob_augment=0.25):
+def data_augmentation(X_train, y_train, brightness, bspline):
     """
     Input:
     ---
@@ -18,14 +18,13 @@ def data_augmentation(X_train, y_train, prob_augment=0.25):
     
     brightness_range = [-0.2, 0.2]
 
-    for idx, (im, seg) in enumerate(zip(X_train_copy, y_train_copy)):
-        # Only augment part of the training set
-        if np.random.rand() <= prob_augment:
-            
+    for idx, (im, seg) in enumerate(zip(X_train_copy, y_train_copy)): 
+        if brightness:
             # Apply random brightness adjustment
             brightness_offset = np.random.uniform(low=brightness_range[0], high=brightness_range[1])
             im = np.clip(im + brightness_offset, 0, 1)  # Ensuring pixel values remain between 0 and 1
 
+        if bspline:
             # Define interpolator objects
             image_interpolator_r = gryds.Interpolator(im[..., 0], order=3)
             image_interpolator_g = gryds.Interpolator(im[..., 1], order=3)
@@ -33,8 +32,8 @@ def data_augmentation(X_train, y_train, prob_augment=0.25):
             segment_interpolator = gryds.Interpolator(seg.squeeze(), order=3)
 
             # Define random displacement
-            disp_x = np.random.uniform(low=-0.075, high=0.075, size=(3, 3))
-            disp_y = np.random.uniform(low=-0.075, high=0.075, size=(3, 3))
+            disp_x = np.random.uniform(low=-0.05, high=0.05, size=(3, 3))
+            disp_y = np.random.uniform(low=-0.05, high=0.05, size=(3, 3))
 
             # Define bspline transformation object
             bspline_transformation = gryds.BSplineTransformation([disp_x, disp_y])
@@ -46,14 +45,15 @@ def data_augmentation(X_train, y_train, prob_augment=0.25):
             augmented_segment = segment_interpolator.transform(bspline_transformation)
 
             # Construct the images correctly
-            augmented_image = np.concatenate((np.expand_dims(augmented_image_r, -1),
-                                              np.expand_dims(augmented_image_g, -1),
-                                              np.expand_dims(augmented_image_b, -1)),
-                                             axis=-1)
+            im = np.concatenate((np.expand_dims(augmented_image_r, -1),
+                                 np.expand_dims(augmented_image_g, -1),
+                                 np.expand_dims(augmented_image_b, -1)),
+                                 axis=-1)
             augmented_segment = np.round(augmented_segment)
+            seg = np.expand_dims(augmented_segment, -1)
 
-            # Place augmented image back into patch array
-            X_train_copy[idx, ...] = augmented_image
-            y_train_copy[idx, ...] = np.expand_dims(augmented_segment, -1)
+        # Place augmented image back into patch array
+        X_train_copy[idx, ...] = im
+        y_train_copy[idx, ...] = seg
 
     return X_train_copy, y_train_copy
